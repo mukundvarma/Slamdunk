81612e95400639bde744827fce691334424e3073
Christopher McGinn <cfmcginn@Christophers-MacBook-Pro.local>
2014-12-21 14:06:21 -0500

    Added sep. file for teamvar

---
+++ BBallTeamVar.h
@@ -0,0 +1,40 @@
+#ifndef BBallTeamVar_h
+#define BBallTeamVar_h
+
+enum TeamNum{
+  ATL,  //0
+  BOS,  //1
+  BRK,  //2
+  CHI,  //3
+  CH0,  //4
+  CLE,  //5
+  DAL,  //6
+  DEN,  //7
+  DET,  //8
+  GSW,  //9
+  HOU,  //10
+  IND,  //11
+  LAC,  //12
+  LAL,  //13
+  MEM,  //14
+  MIA,  //15
+  MIL,  //16
+  MIN,  //17
+  NOP,  //18
+  NYK,  //19
+  OKC,  //20
+  ORL,  //21
+  PHI,  //22
+  PHO,  //23
+  POR,  //24
+  SAC,  //25
+  SAS,  //26
+  TOR,  //27
+  UTA,  //28
+  WAS   //29
+};
+
+const Int_t nTeams = 30;
+const std::string teams[nTeams] = {"ATL", "BOS", "BRK", "CHI", "CHO", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"};
+
+#endif
--- BBallTrees.h
+++ BBallTrees.h
@@ -1,7 +1,7 @@
 //=============================================                                 
 // Author: Chris McGinn                                                         
 //                                                                              
-// BBall Trees Class (MC)                                                    
+// BBall Trees Class                                                    
 //                                                                              
 //=============================================  
 
@@ -13,45 +13,10 @@
 #include "TFile.h"
 #include "TTree.h"
 
-enum TeamNum{
-  ATL,  //0
-  BOS,  //1
-  BRK,  //2
-  CHI,  //3
-  CH0,  //4
-  CLE,  //5
-  DAL,  //6
-  DEN,  //7
-  DET,  //8
-  GSW,  //9
-  HOU,  //10
-  IND,  //11
-  LAC,  //12
-  LAL,  //13
-  MEM,  //14
-  MIA,  //15
-  MIL,  //16
-  MIN,  //17
-  NOP,  //18
-  NYK,  //19
-  OKC,  //20
-  ORL,  //21
-  PHI,  //22
-  PHO,  //23
-  POR,  //24
-  SAC,  //25
-  SAS,  //26
-  TOR,  //27
-  UTA,  //28
-  WAS   //29
-};
-
+#include "BBallTeamVar.h"
 
 TTree* statTree_p = 0;
 TTree* bookTree_p = 0;
-
-const Int_t nTeams = 30;
-const std::string teams[nTeams] = {"ATL", "BOS", "BRK", "CHI", "CHO", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOP", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"};
 
 const Int_t maxGames = 130;
 
