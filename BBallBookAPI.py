b7860090ad349153c2be0d66f8dc18c31488e0da
Christopher McGinn <cfmcginn@Christophers-MacBook-Pro.local>
2014-12-22 16:34:59 -0500

    Prototype book api written

--- BBallBookAPI.py
+++ BBallBookAPI.py
@@ -18,10 +18,26 @@ def getSoupFromURL(url, supressOutput=True):
     
     return BeautifulSoup(r.text)
 
-#bball = getSoupFromURL('http://www.vegasinsider.com/nba/scoreboard/scores.cfm/game_date/12-20-2014', True)
-bball = getSoupFromURL('http://www.basketball-reference.com/teams/ATL/2015_games.html', True)
+bball = getSoupFromURL('http://www.vegasinsider.com/nba/scoreboard/scores.cfm/game_date/12-20-2014', True)
 
-print bball
+for thing in bball.findAll('tr'):
+  counter = 0
+  if str(thing)[11:16] == 'tanBg':
+    for thing2 in thing.findAll('td'):
+      if counter == 2:
+        start = str(thing2).index('>')  
+        end = str(thing2)[1:].index('<')
+
+        if str(thing2)[start+3:start+4] == '-':
+          print "Odds: " + str(thing2)[start+3:end-1]
+        else:
+          print "Over/Under: " + str(thing2)[start+3:end-1]
+
+      elif counter > 2:
+        break
+      counter += 1
+
+#print bball
 
 #for thing in bball.findAll()
  # outFile.write()
